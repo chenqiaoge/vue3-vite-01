b408cc43cb5457f7524dc510f26f58beefe38e6a
陈桥格 <qiaoge.chen@ikang.com>
2020-07-16 10:43:47 +0800

    composition逻辑拆分的优势

--- src/components/counts.vue
+++ src/components/counts.vue
@@ -10,20 +10,12 @@ import { ref,reactive, computed,onMounted } from 'vue'
 export default {
   // setup是新的选项，可以理解是comopsition的入口，函数内部在beforeCreate之前调用，函数返回的内容会作为模板渲染的上下文
   setup () {
-    const state = reactive({
-      count: 1
-    })
-    //reactive负责复杂数据结构，ref可以把基本的数据结构包装成响应式
-    // ref包装的num，模板里可以直接写，js中修改的时候操作.value属性
-    let num = ref(1)
-
+    const {state,double,num} = moduleFunc(1,2)
     function add () {
       // console.log(state,num.value)
       state.count++;
       num.value+=5
     }
-    const double = computed(() => state.count * 2)
-
     onMounted(()=>{
       console.log('mouted')
     })
@@ -31,6 +23,15 @@ export default {
   }
 }
 
-// 拆分函数
-
+// 拆分函数(完全可以独立文件拆分出去部分逻辑)
+function moduleFunc(initCount,initNum){
+  const state = reactive({
+      count: initCount
+  })
+  //reactive负责复杂数据结构，ref可以把基本的数据结构包装成响应式
+  // ref包装的num，模板里可以直接写，js中修改的时候操作.value属性
+  let num = ref(initNum)
+  const double = computed(() => state.count * 2)
+  return {state,double,num}
+}
 </script>
